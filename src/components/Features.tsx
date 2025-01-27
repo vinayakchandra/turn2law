4818a2bd94a6c998d4ce2a275143eadba72d70b9
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2025-01-28 01:31:18 +0530

    handling "any" Type Error

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 
-function Features(props: { callbackfn: (feature, index:number) => React.JSX.Element }) {
+interface Feature {
+  img: string;
+  text: string;
+}
+
+function Features(props: { callbackfn: (feature: Feature, index: number) => React.JSX.Element }) {
+  const features: Feature[] = [
+    { img: "/hero/FaHandshake.svg", text: "Rental and Lease Agreements" },
+    { img: "/hero/FaBookOpen.svg", text: "Legal Opinions" },
+    { img: "/hero/IoPersonOutline.svg", text: "Wills & Trusts" },
+    { img: "/hero/Nda.svg", text: "Non-Disclosure Agreements" },
+    { img: "/hero/VscLaw.svg", text: "Others" },
+  ];
+
   return (
     <div className="relative w-full px-6 md:px-12 mt-8">
       <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-6">
-        {[
-          { img: "/hero/FaHandshake.svg", text: "Rental and Lease Agreements" },
-          { img: "/hero/FaBookOpen.svg", text: "Legal Opinions" },
-          { img: "/hero/IoPersonOutline.svg", text: "Wills & Trusts" },
-          { img: "/hero/Nda.svg", text: "Non-Disclosure Agreements" },
-          { img: "/hero/VscLaw.svg", text: "Others" },
-        ].map(props.callbackfn)}
+        {features.map(props.callbackfn)}
       </div>
     </div>
   );
