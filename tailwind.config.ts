ebbb490ff085bf17cde1b47a349d7c12447d4a92
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2025-01-28 01:20:54 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,30 @@
 import type { Config } from "tailwindcss";
 
+import tailwind_scrollbar from "tailwind-scrollbar";
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // background: "var(--background)",
+        // foreground: "var(--foreground)",
+        text: "#000", // Custom text color
+        primary: "#DF9C49",
+        secondary: "#AE7739",
+        tertiary: "#17726E",
+        background: "#FFF",
+        border: "#C2C2C2",
       },
+      ringColor: {
+        tertiary: "#17726E",
+      },
+
     },
   },
   plugins: [],
