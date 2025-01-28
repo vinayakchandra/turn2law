fa4835c1ac1f0972c335fb0ace864a4e81733305
Vinayak Chandra Suryavanshi <vinayak.chandra.suryavanshi@gmail.com>
2025-01-28 21:16:44 +0530

    fixing divider

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -4,17 +4,15 @@ import React from 'react';
 const OnboardingForm = () => {
   return (
     <div className="m-6 md:m-10 mb-6">
-        <img src="/Logo.svg"
-             alt="logo"
-             className="h-10"
-             onError={(e) => {
-               console.error("Error loading image:", e);
-             }}
-        />
-      <div
-        className="grid grid-cols-1 divide-x-1 divide-gray-300 md:grid-cols-2 min-h-screen rounded-2xl overflow-hidden gap-4 md:gap-8">
-        {/* Left Section */}
-        <div className="flex flex-col justify-center items-center p-8 rounded-bl-2xl md:rounded-l-2xl overflow-hidden">
+      <img
+        src="/Logo.svg"
+        alt="logo"
+        className="h-10"
+      />
+      <div className="grid grid-cols-1 min-h-[90vh] md:grid-cols-2  overflow-hidden gap-4 md:gap-8">
+
+      {/* Left Section */}
+        <div className="flex flex-col justify-center items-center p-8 overflow-hidden border-b md:border-b-0 md:border-r border-gray-300">
           <div className="text-left">
             <h1 className="text-4xl font-bold">
               Let&#39;s get to know <br/>
@@ -24,12 +22,12 @@ const OnboardingForm = () => {
               <input
                 type="text"
                 placeholder="Name"
-                className="w-full px-4 py-2 bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-tertiary"
+                className="w-full px-4 py-2 bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
               />
               <input
                 type="text"
                 placeholder="+91"
-                className="w-full px-4 py-2 bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-tertiary"
+                className="w-full px-4 py-2 bg-gray-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
               />
               <input
                 type="text"
@@ -46,8 +44,7 @@ const OnboardingForm = () => {
         </div>
 
         {/* Right Section */}
-        <div
-          className="bg-white flex flex-col justify-center items-center p-8 rounded-tr-2xl md:rounded-r-2xl overflow-hidden ">
+        <div className="bg-white flex flex-col justify-center items-center p-8 overflow-hidden">
           <div className="w-full max-w-md">
             <h2 className="text-2xl font-semibold mb-6 text-center">Service type</h2>
             <div className="grid grid-cols-2 gap-4">
